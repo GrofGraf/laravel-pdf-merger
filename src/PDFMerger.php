917868cfcdeb4a9516e045c7ce97e4fdb08473f1
Axel Piaget <axel.piaget@hotmail.com>
2020-05-23 14:46:16 +0200

    compatibility with Laravel 6+

--- src/PDFMerger.php
+++ src/PDFMerger.php
@@ -5,6 +5,7 @@ namespace GrofGraf\LaravelPDFMerger;
 use setasign\Fpdi\Fpdi;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
+use Str;
 
 class PDFMerger {
     /**
@@ -117,7 +118,7 @@ class PDFMerger {
      * @return void
      */
     public function addPDFString($string, $pages = 'all', $orientation = null){
-        $filePath = storage_path('tmp/'.str_random(16).'.pdf');
+        $filePath = storage_path('tmp/'. Str::random(16).'.pdf');
         $this->filesystem->put($filePath, $string);
         $this->tmpFiles->push($filePath);
         return $this->addPathToPDF($filePath, $pages, $orientation);
@@ -208,7 +209,7 @@ class PDFMerger {
       preg_match_all('!\d+!', $first_line, $matches);
       $pdfversion = implode('.', $matches[0]);
       if($pdfversion > "1.4"){
-        $newFilePath = storage_path('tmp/' . str_random(16) . '.pdf');
+        $newFilePath = storage_path('tmp/' . Str::random(16) . '.pdf');
         //execute shell script that converts PDF to correct version and saves it to tmp folder
         shell_exec('gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="'. $newFilePath . '" "' . $filePath . '"');
         $this->tmpFiles->push($newFilePath);
